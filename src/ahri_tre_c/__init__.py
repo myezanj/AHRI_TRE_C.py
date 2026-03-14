97503a8d4b4c2ca54b83de75255965b78078e5f7
Myeza <34543679+myezanj@users.noreply.github.com>
2026-03-14 14:17:57 +0200

    Update __init__.py

--- src/ahri_tre_c/__init__.py
+++ src/ahri_tre_c/__init__.py
@@ -1,3 +1,11 @@
-from .core import AHRI_TRE_C, default_library_path
+from . import core as _core
 
-__all__ = ["AHRI_TRE_C", "default_library_path"]
+_export_names = list(_core.__all__)
+for _name in sorted(set(_core._REQUIRED_API_NAMES)):
+	if _name.isidentifier() and _name not in _export_names:
+		_export_names.append(_name)
+
+for _name in _export_names:
+	globals()[_name] = getattr(_core, _name)
+
+__all__ = _export_names
