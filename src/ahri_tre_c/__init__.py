00be3153248f792f5df74d4587928d8985c2dd8a
Myeza <34543679+myezanj@users.noreply.github.com>
2026-03-14 16:40:01 +0200

    Update __init__.py

--- src/ahri_tre_c/__init__.py
+++ src/ahri_tre_c/__init__.py
@@ -1,8 +1,8 @@
 from . import core as _core
 
 _export_names = list(_core.__all__)
-for _name in sorted(set(_core._REQUIRED_API_NAMES)):
-	if _name.isidentifier() and _name not in _export_names:
+for _name in _core._MODULE_PROXY_NAMES:
+	if _name not in _export_names:
 		_export_names.append(_name)
 
 for _name in _export_names:
